1c0ec8c88236b58058052d1444504fb6caadc38c
Orest Dubay <orest3.dubay@gmail.com>
2021-02-18 13:57:17 +0100

    multiple resources created with all (configured) situation reports

--- run.py
+++ run.py
@@ -11,34 +11,47 @@ from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import progress_storing_tempdir
 
-from unicef import generate_dataset_and_showcase, get_countriesdata
+from unicef import generate_dataset_and_showcase, get_all_countriesdata
 
 from hdx.facades.simple import facade
 
 logger = logging.getLogger(__name__)
 
 
-lookup = 'hdx-scraper-unicef-sam'
+lookup = "hdx-scraper-unicef-sam"
 
 
 def main():
     """Generate dataset and create it in HDX"""
 
     with Download() as downloader:
-        url = Configuration.read()['CV_03_04']['url']
-        countries, countriesdata, headers = get_countriesdata(url, downloader)
-        logger.info('Number of datasets to upload: %d' % len(countries))
-        for info, country in progress_storing_tempdir('UNICEFSAM', countries, 'iso3'):
-            dataset, showcase = generate_dataset_and_showcase(info['folder'], country, countriesdata[country['iso3']], headers)
+        project_config = {key:value for key,value in Configuration.read().items() if key.startswith("CV")}
+        countries, countriesdata, headers = get_all_countriesdata(
+                project_config, downloader
+            )
+
+        logger.info("Number of datasets to upload: %d" % len(countries))
+        for info, country in progress_storing_tempdir("UNICEFSAM", countries, "iso3"):
+            dataset, showcase = generate_dataset_and_showcase(
+                info["folder"], country, countriesdata[country["iso3"]], headers, project_config
+            )
             if dataset:
                 dataset.update_from_yaml()
-                dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False, updated_by_script='HDX Scraper: UNICEF Sam', batch=info['batch'])
-                #dataset.generate_resource_view()
+                dataset.create_in_hdx(
+                    remove_additional_resources=True,
+                    hxl_update=False,
+                    updated_by_script="HDX Scraper: UNICEF Sam",
+                    batch=info["batch"],
+                )
+                # dataset.generate_resource_view()
                 showcase.create_in_hdx()
                 showcase.add_dataset(dataset)
 
 
-if __name__ == '__main__':
-    facade(main, user_agent_config_yaml=join(expanduser('~'), '.useragents.yml'), user_agent_lookup=lookup, project_config_yaml=join('config', 'project_configuration.yml'))
-
-
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yml"),
+        user_agent_lookup=lookup,
+        project_config_yaml=join("config", "project_configuration.yml"),
+    )
--- unicef.py
+++ unicef.py
@@ -38,32 +38,56 @@ hxltags = {
 
 WORLD = "world"
 
+
 def get_countriesdata(url, downloader, with_world=True):
+    """Fetch the countries data from an url and split them by country."""
     headers, iterator = downloader.get_tabular_rows(url, dict_form=True)
-    countriesset = set()
     countriesdata = dict()
-    if with_world:
-        countriesset.add(WORLD)
     for row in iterator:
         countryiso3 = row["REF_AREA"]
-        countriesset.add(countryiso3)
         countriesdata[countryiso3] = countriesdata.get(countryiso3, []) + [row]
         if with_world:
             countriesdata[WORLD] = countriesdata.get(WORLD, []) + [row]
 
+    return countriesdata, headers
+
+
+def countries_from_iso_list(countriesset):
+    """
+    Create a list of dictionaries describing each country in the countriesset.
+    The countriesset is a list or set of iso3 country identifiers.
+    Output list contains a dictionary with "iso3" and "name" of a country.
+    """
     countries = list()
     for countryiso in sorted(list(countriesset)):
-        if countryiso==WORLD:
+        if countryiso == WORLD:
             countries.append({"iso3": WORLD, "name": "World"})
         else:
             countryname = Country.get_country_name_from_iso3(countryiso)
             if countryname is None:
                 continue
             countries.append({"iso3": countryiso, "name": countryname})
-    return countries, countriesdata, headers
+    return countries
 
 
-def generate_dataset_and_showcase(folder, country, countrydata, headers):
+def get_all_countriesdata(config, downloader, with_world=True):
+    countriesset = set()
+    countriesdata = {}
+    headers = {}
+    for report_id, report_config in config.items():
+        logger.info("Getting situation report %s" % report_id)
+        url = report_config["url"]
+        report_countriesdata, report_headers = get_countriesdata(url, downloader)
+        countriesset.update(report_countriesdata.keys())
+        headers[report_id] = report_headers
+        for countryiso, data in report_countriesdata.items():
+            countriesdata[countryiso] = countriesdata.get(countryiso,{})
+            countriesdata[countryiso][report_id] = data
+
+    return countries_from_iso_list(countriesset), countriesdata, headers
+
+
+def generate_dataset_and_showcase(folder, country, countrydata, headers, config):
     countryname = country["name"]
     countryiso = country["iso3"].lower()
     if countryiso == WORLD:
@@ -71,15 +95,25 @@ def generate_dataset_and_showcase(folder, country, countrydata, headers):
     else:
         title = "%s - COVID-19 Situation Report" % country["name"]
     logger.info("Creating dataset: %s" % title)
-    name = "UNICEF SAM COVID-19 indicators for %s" % country["name"]
+    name = "UNICEF COVID-19 situation report indicators for %s" % country["name"]
     slugified_name = slugify(name).lower()
     dataset = Dataset({"name": slugified_name, "title": title,})
-#    dataset.set_maintainer("085d3bd8-9035-4b0e-9d2d-80e849dd7b07")
+    #    dataset.set_maintainer("085d3bd8-9035-4b0e-9d2d-80e849dd7b07")
     dataset.set_maintainer("9957c0e9-cd38-40f1-900b-22c91276154b")
     dataset.set_organization("3ab17ac1-1196-4501-a4dc-a01d2e52ff7c")
     dataset.set_subnational(False)
     dataset.set_expected_update_frequency("Every month")
-    dataset.add_tags(["hxl", "children", "COVID-19", "malnutrition"])
+    dataset.add_tags(
+        [
+            "hxl",
+            "children",
+            "COVID-19",
+            "malnutrition",
+            "hygiene",
+            "health",
+            "healthcare",
+        ]
+    )
 
     if countryiso == WORLD:
         dataset.add_other_location("world")
@@ -90,35 +124,47 @@ def generate_dataset_and_showcase(folder, country, countrydata, headers):
             logger.error(f"{countryname} ({countryiso})  not recognised!")
             return None, None
 
-    filename = "covid19sitrep_%s.csv" % countryiso
-    resourcedata = {
-        "name": "COVID-19 indicators for %s" % countryname,
-        "description": "COVID-19 Situation Report",
-        "countryiso":countryiso,
-        "countryname":countryname
-    }
-    success, results = dataset.generate_resource_from_iterator(
-        headers,
-        countrydata,
-        hxltags,
-        "out",#folder,
-        filename,
-        resourcedata,
-        datecol="TIME_PERIOD",
-    )
-    if success is False:
-        logger.warning("%s has no data!" % name)
-        return None, None
+    for report_id in countrydata.keys():
+        resource_config = config[report_id]
+        filename = resource_config["filename"] + "_%s.csv" % countryiso
+        logger.info("Creating resource %s for report %s in %s" % (filename,report_id,countryiso))
+        resourcedata = {
+            "name": "%s - %s" % (resource_config["name"], countryname),
+            "description": resource_config["description"],
+            "countryiso": countryiso,
+            "countryname": countryname,
+        }
+        success, results = dataset.generate_resource_from_iterator(
+            headers[report_id],
+            countrydata[report_id],
+            hxltags,
+            "out",  # folder,
+            filename,
+            resourcedata,
+            datecol="TIME_PERIOD",
+        )
+        if success is False:
+            logger.warning("%s has no data!" % name)
+            return None, None
 
     showcase = Showcase(
         {
             "name": "%s-showcase" % slugified_name,
             "title": name,
-            "notes": "Cases of severe acute malnutrition (SAM) and actions taken.",
+            "notes": "UNICEF Situation Reports for COVID19 including severe acute malnutrition (SAM) and actions taken.",
             "url": "https://sites.unicef.org/nutrition/index_sam.html",
             "image_url": "https://sites.unicef.org/includes/images/unicef_for-every-child_EN.png",
         }
     )
-    showcase.add_tags(["hxl", "children", "COVID-19", "malnutrition"])
+    showcase.add_tags(
+        [
+            "hxl",
+            "children",
+            "COVID-19",
+            "malnutrition",
+            "hygiene",
+            "health",
+            "healthcare",
+        ]
+    )
     return dataset, showcase
-
